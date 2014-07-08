b659a1a8a71dd927f0bdf35cc79174b0c8d6745e
jjsmith <jjsmith@agiledigital.com.au>
2014-07-08 15:42:58 +1000

    Ensure defaults are loaded at startup
    Make it so the user never has to edit a setting in the defaults panel
    of the App. This will load the defaults for any setting they have not
    yet changed.

--- Sentinel/Sentinel/GASAppDelegate.m
+++ Sentinel/Sentinel/GASAppDelegate.m
@@ -14,8 +14,11 @@
 {
     // Override point for customization after application launch.
     
-    // Settings bundle does not copy to user defaults unless user tweaks each value. This is a quick fix.
+    // Settings bundle does not copy to user defaults unless user tweaks each value.
     [[NSUserDefaults standardUserDefaults] registerDefaults:[NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"SettingsDefaults" ofType:@"plist"]]];
+    
+    // Ensure we have the settings loaded without the user having to go and edit a slider in Settings
+    [self registerDefaultsFromSettingsBundle];
     
     return YES;
 }
@@ -49,6 +52,51 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+- (void)registerDefaultsFromSettingsBundle
+{
+    NSLog(@"Registering default values from Settings.bundle");
+    NSUserDefaults * defs = [NSUserDefaults standardUserDefaults];
+    [defs synchronize];
+    
+    NSString *settingsBundle = [[NSBundle mainBundle] pathForResource:@"Settings" ofType:@"bundle"];
+    
+    if(!settingsBundle)
+    {
+        NSLog(@"Could not find Settings.bundle");
+        return;
+    }
+    
+    NSDictionary *settings = [NSDictionary dictionaryWithContentsOfFile:[settingsBundle stringByAppendingPathComponent:@"Root.plist"]];
+    NSArray *preferences = [settings objectForKey:@"PreferenceSpecifiers"];
+    NSMutableDictionary *defaultsToRegister = [[NSMutableDictionary alloc] initWithCapacity:[preferences count]];
+    
+    for (NSDictionary *prefSpecification in preferences)
+    {
+        NSString *key = [prefSpecification objectForKey:@"Key"];
+        if (key)
+        {
+            // check if value readable in userDefaults
+            id currentObject = [defs objectForKey:key];
+            if (currentObject == nil)
+            {
+                // not readable: set value from Settings.bundle
+                id objectToSet = [prefSpecification objectForKey:@"DefaultValue"];
+                [defaultsToRegister setObject:objectToSet forKey:key];
+                NSLog(@"Setting object %@ for key %@", objectToSet, key);
+            }
+            else
+            {
+                // already readable: don't touch
+                NSLog(@"Key %@ is readable (value: %@), nothing written to defaults.", key, currentObject);
+            }
+        }
+    }
+    
+    [defs registerDefaults:defaultsToRegister];
+    [defs synchronize];
 }
 
 @end
--- Sentinel/Sentinel/GASHomeViewController.m
+++ Sentinel/Sentinel/GASHomeViewController.m
@@ -23,7 +23,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        // Custom Init
     }
     return self;
 }
